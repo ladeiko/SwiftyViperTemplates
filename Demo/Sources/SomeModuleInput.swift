9fd8a9bd9e1ec61d265eb6e22cda13495717ee2f
Developer S <s.developer@modules.tools>
2023-01-09 18:18:42 +0300

    removed storyboard support, optimizations

--- Demo/Sources/SomeModuleInput.swift
+++ Demo/Sources/SomeModuleInput.swift
@@ -8,6 +8,7 @@
 
 import ViperMcFlurryX
 
+@MainActor
 protocol SomeModuleInput: AnyObject {
     func configure()
 }
---
+++ dev_copy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+rm -f "${HOME}/.viperaptor/catalogs/SwiftyViperTemplates/*.rambaspec"
+cp -R ./* "${HOME}/.viperaptor/catalogs/SwiftyViperTemplates/"
